b1d69da46310b59c3a697d3aab9e29f586e1045e
kakserpom <kak.serpom.po.yaitsam@gmail.com>
2011-02-01 06:03:46 +0300

    Fixed get_query modifier.

--- plugins/modifier.get_query.php
+++ plugins/modifier.get_query.php
@@ -1,17 +1,34 @@
 <?php
 function quicky_modifier_get_query($params,$mode = '')
 {
- $q = '';
- $all = strpos($mode,'a') !== FALSE;
- $plain = strpos($mode,'p') !== FALSE;
- $d = $plain?'&':'&amp;';
- if ($all) {$params = array_unique(array_merge(array_keys($_GET),$params));}
- foreach ($params as $k => $v)
- {
-  $q .= is_int($k)
-			?(isset($_REQUEST[$v]) && (!array_key_exists($v,$params))?($q !== ''?$d:'?').$v.'='.urlencode(gpcvar_str($_REQUEST[$v])):'')
-			:($v !== NULL?($q !== ''?$d:'?').$k.'='.urlencode($v):'')
-  ;
- }
- return $q;
+    $query           = '';
+    $all             = strpos($mode,'a') !== FALSE;
+    $plain           = strpos($mode,'p') !== FALSE;
+    $separator       = $plain?'&':'&amp;';
+    $params_from_get = array();
+    $params_sended   = array();
+    foreach ($params as $k => $v){
+        if (is_int($k) and isset($_GET[$k]))
+            $params_from_get[$k]=$_GET[$k];
+        else{
+            $params_sended[$k]=$v;
+        }
+    }
+    if ($all){
+        foreach ($_GET as $k => $v){
+            if (isset($_REQUEST[$k]))
+                $params_from_get[$k]=$v;
+        }
+    }
+    $params = array_merge($params_from_get, $params_sended);
+    foreach ($params as $k => $v){
+        if ($v === NULL){
+            continue;
+        }
+        $query .= ($query !== ''?$separator:'?').(is_array($v)?
+            http_build_query(array($k => $v), '', $separator):
+            $k.'='.urlencode($v)
+        );
+    }
+    return $query;
 }
